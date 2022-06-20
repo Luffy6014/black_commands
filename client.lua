5ff77e40971db006bbf6c423d4a5204c49e6befb
Luffy6014 <107808984+Luffy6014@users.noreply.github.com>
2022-06-20 13:48:58 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -82,15 +82,13 @@ RegisterCommand("notte", function()
     ESX.ShowNotification("Hai impostato il meteo di notte!")
 end)
 
---how long you want the thing to last for. in seconds.
 announcestring = false
 lastfor = 5
 
---DO NOT TOUCH BELOW THIS LINE OR YOU /WILL/ FUCK SHIT UP.
---DO NOT BE STUPID AND WHINE TO ME ABOUT THIS BEING BROKEN IF YOU TOUCHED THE LINES BELOW.
 RegisterNetEvent('announce')
 announcestring = false
 AddEventHandler('announce', function(msg)
+
 	announcestring = msg
 	PlaySoundFrontend(-1, "DELETE","HUD_DEATHMATCH_SOUNDSET", 1)
 	Citizen.Wait(lastfor * 2000)
